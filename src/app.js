8513c711f27129bd6fdcdc19cc8e4c6439fd8111
AdrianaMagdalena <128493793+AdrianaMagdalena@users.noreply.github.com>
2023-08-22 22:01:58 +0200

    Add draw function and fix typoes in tetrominoes

--- src/app.js
+++ src/app.js
@@ -6,37 +6,57 @@ let squares = Array.from(document.querySelectorAll(".grid div"));
 const width = 10;
 
 // the tetrominoes
-const lTetronimo = [
+const lTetromino = [
   [1, width + 1, width * 2 + 1, 2],
   [width, width + 1, width + 2, width * 2 + 2],
   [1, width + 1, width * 2, width * 2 + 1],
   [width, width * 2, width * 2 + 1, width * 2 + 2],
 ];
 
-const zTetronimo = [
+const zTetromino = [
   [width * 2 + 1, width * 2 + 2, width * 3, width * 3 + 1],
   [1, width, width + 1, width * 2 + 1],
   [width * 2 + 1, width * 2 + 2, width * 3, width * 3 + 1],
   [1, width, width + 1, width * 2 + 1],
 ];
 
-const tTetronimo = [
+const tTetromino = [
   [1, width, width + 1, width + 2],
   [1, width + 1, width + 2, width * 2 + 1],
   [width, width + 1, width + 2, width * 2 + 1],
   [1, width, width + 1, width * 2 + 1],
 ];
 
-const oTetronimo = [
+const oTetromino = [
   [0, 1, width, width + 1],
   [0, 1, width, width + 1],
   [0, 1, width, width + 1],
   [0, 1, width, width + 1],
 ];
 
-const iTetronimo = [
+const iTetromino = [
   [1, width + 1, width * 2 + 1, width * 3 + 1],
   [width, width + 1, width + 2, width + 3],
   [1, width + 1, width * 2 + 1, width * 3 + 1],
   [width, width + 1, width + 2, width + 3],
 ];
+
+const theTetrominoes = [
+  lTetromino,
+  zTetromino,
+  tTetromino,
+  oTetromino,
+  iTetromino,
+];
+
+let currentPosition = 4;
+let current = theTetrominoes[0][0];
+console.log(current);
+
+// draw the first rotation in the first tetronimmo
+function draw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.add("tetromino");
+  });
+}
+draw();
