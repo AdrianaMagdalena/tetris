a882edc002eb029d77881f352c3e2fee250f085e
AdrianaMagdalena <128493793+AdrianaMagdalena@users.noreply.github.com>
2023-08-23 11:00:45 +0200

    Make blocks move right, left, down faster and rotate

--- src/app.js
+++ src/app.js
@@ -15,9 +15,9 @@ const lTetromino = [
 
 const zTetromino = [
   [width + 1, width + 2, width * 2, width * 2 + 1],
-  [1, width, width + 1, width * 2 + 1],
+  [1, width + 1, width + 2, width * 2 + 2],
   [width + 1, width + 2, width * 2, width * 2 + 1],
-  [1, width, width + 1, width * 2 + 1],
+  [1, width + 1, width + 2, width * 2 + 2],
 ];
 
 const tTetromino = [
@@ -76,6 +76,21 @@ function undraw() {
 // make the tetromino to move down every second
 timerID = setInterval(moveDown, 500);
 
+//assign functions to keyCode:
+function control(e) {
+  if (e.key === "ArrowLeft") {
+    moveLeft();
+  } else if (e.key === "ArrowRight") {
+    moveRight();
+  } else if (e.key === "ArrowUp") {
+    rotate();
+  } else if (e.key === "ArrowDown") {
+    moveDown();
+  }
+}
+
+document.addEventListener("keyup", control);
+
 function moveDown() {
   undraw();
   currentPosition += width;
@@ -98,4 +113,53 @@ function freeze() {
     currentPosition = 4;
     draw();
   }
+}
+
+//move the tetromino left until it reaches the edge:
+function moveLeft() {
+  undraw();
+  const isAtLeftEdge = current.some(
+    (index) => (currentPosition + index) % width === 0
+  );
+  if (!isAtLeftEdge) {
+    currentPosition -= 1;
+  }
+  if (
+    current.some((index) =>
+      squares[currentPosition + index].classList.contains("taken")
+    )
+  ) {
+    currentPosition += 1;
+  }
+  draw();
+}
+
+function moveRight() {
+  undraw();
+  const isAtRightEdge = current.some(
+    (index) => (currentPosition + index) % width === width - 1
+  );
+  if (!isAtRightEdge) {
+    currentPosition += 1;
+  }
+  if (
+    current.some((index) =>
+      squares[currentPosition + index].classList.contains("taken")
+    )
+  ) {
+    currentPosition -= 1;
+  }
+  draw();
+}
+
+//rotate the teromino
+function rotate() {
+  undraw();
+  currentRotation++;
+  //if the current rotation goes to 4, make it go back to 0
+  if (currentRotation === current.length) {
+    currentRotation = 0;
+  }
+  current = theTetrominoes[random][currentRotation];
+  draw();
 }
